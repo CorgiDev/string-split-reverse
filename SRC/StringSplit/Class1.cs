bb10d238f425e4627d64b13ca13735d4c6f7f898
EGrayTech <EGrayTech@users.noreply.github.com>
2018-03-06 01:33:02 -0500

    Created the class to perform the actions necessary and a unit test to test that it worked properly.

---
+++ SRC/StringSplit/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringSplit
+{
+    public class Class1
+    {
+        public string SplitString(string input)
+        {
+            string reversedPhrase ="";
+            string phrase = "The cat goes";
+            string[] words = phrase.Split(' ');
+            foreach (string word in words)
+            {
+                string reversedWord = new string(word.Reverse().ToArray());
+                reversedPhrase += reversedWord;
+                reversedPhrase += " ";
+            }
+            //Return the array back to a string.
+            return reversedPhrase.Trim();
+        }
+    }
+}
---
+++ SRC/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringSplit;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Class1 _split;
+        [TestInitialize]
+        public void Init()
+        {
+            _split = new Class1();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var output = _split.SplitString("The cat goes");
+            Assert.AreEqual("ehT tac seog", output);
+        }
+    }
+}
